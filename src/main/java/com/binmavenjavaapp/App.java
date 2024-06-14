d78d3fde012a9d750cef738bd589291cf4ec46b2
Sanket <sanketjambharunkar@gmail.com>
2024-06-14 16:40:40 +0530

    java app v1.0

---
+++ src/main/java/com/binmavenjavaapp/App.java
@@ -0,0 +1,13 @@
+package com.binmavenjavaapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
